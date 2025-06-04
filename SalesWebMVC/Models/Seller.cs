9b43e1103b350de079bde0d2b11d4cb4fa935eb9
Karoline <karolinerebeca96@gmail.com>
2025-06-04 19:56:20 -0300

    Other entities ans second migration

---
+++ SalesWebMVC/Models/Seller.cs
@@ -0,0 +1,43 @@
+﻿namespace SalesWebMVC.Models;
+
+public class Seller
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; } 
+    public DateTime BirthDay { get; set; }
+    public double BaseSalary { get; set; }
+    public Department? Department { get; set; }
+    public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+
+    public Seller() 
+    {
+    }
+
+    public Seller(int id, string? name, string? email, DateTime birthDay, double baseSalary, Department? department)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+        BirthDay = birthDay;
+        BaseSalary = baseSalary;
+        Department = department;
+    }
+
+    public void AddSales(SalesRecord sr)
+    {
+        Sales.Add(sr);
+    }
+
+    public void RemoveSales(SalesRecord sr)
+    { 
+        Sales.Remove(sr); 
+    }
+
+    public double TotalSales(DateTime Initial, DateTime Final) 
+    {
+        return Sales.Where(sr => sr.Date >= Initial && sr.Date <= Final) .Sum(sr => sr.Amount);
+    }
+
+}
