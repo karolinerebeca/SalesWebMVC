277231c98b9b3be8344d732134c3375abeeb2132
Karoline <karolinerebeca96@gmail.com>
2025-07-23 12:13:02 -0300

    Implenting grouping search

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -1,31 +1,33 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace SalesWebMVC.Models;
-
-public class Department
+namespace SalesWebMVC.Models
 {
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
-
-
-    public Department()
+    public class Department
     {
-    }
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
-    public Department(int id, string? name)
-    {
-        Id = id;
-        Name = name;
-    }
+        public Department()
+        {
+        }
 
-    public void AddSeller(Seller seller)
-    {
-        Sellers.Add(seller);
-    }
+        public Department(int id, string? name)
+        {
+            Id = id;
+            Name = name;
+        }
 
-    public double TotalSales(DateTime initial, DateTime final)
-    {
-        return Sellers.Sum(seller => TotalSales(initial, final));
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
